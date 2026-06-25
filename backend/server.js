3716bfb798166acecf9cfed948c6f66b3f894458
prerans <preran248@gmail.com>
2026-06-25 18:06:32 +0530

    changed and corrected :)

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    'https://alloydocs-2.onrender.com',
     'https://alloy-docs-tms2.vercel.app'
   ],
   credentials: true,
